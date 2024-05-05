10315a1cd7a8e526e846d1d3157e75bdebd7d67d
vinicioscsoares <vinicioscsoares@gmail.com>
2024-05-05 15:00:54 -0300

    setup completo

--- Navezinha/src/main.cpp
+++ Navezinha/src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main()
 {
+	sf::RenderWindow renderWindow(sf::VideoMode(200, 200), "Meu Joguinho");
 	cout << "Ola Mundo" << endl;
 	return 0;
 }
